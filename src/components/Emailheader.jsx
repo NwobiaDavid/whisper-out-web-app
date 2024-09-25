ad72418c9d8c5a4f7d4e2a6dfef202994727fabc
NwobiaDavid <dnwobia@gmail.com>
2024-09-26 00:17:49 +0100

    feat: added the state management for the theme toggle

--- src/components/Emailheader.jsx
+++ src/components/Emailheader.jsx
@@ -1,12 +1,40 @@
-import { Image } from '@nextui-org/image'
+import { Image } from '@nextui-org/image';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleDarkMode } from '../state/theme/themeSlice';
+import { useEffect } from 'react';
 
 const Emailheader = () => {
-  return (
-    <div className=' w-full p-5 ' >
-    <Image className=' w-[60px] ' alt="Whisper out logo" src={'/assets/logo1.png'} />
-    </div>
-  )
-}
 
-export default Emailheader
+    const dispatch = useDispatch();
+    const darkMode = useSelector((state) => state.theme.darkMode);
+  
+    useEffect(() => {
+      if (darkMode) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    }, [darkMode]);
+
+  return (
+    <div className=" relative w-full px-3 py-10 bg-white dark:bg-slate-900 flex justify-center items-center ">
+      <Image
+        className=" w-[60px] "
+        alt="Whisper out logo"
+        src={'/assets/logo1.png'}
+      />
+
+      <div className="absolute bottom-[30%] left-[7%] ">
+      <button
+        onClick={() => dispatch(toggleDarkMode())}
+        className="px-4 py-2 border border-gray-600 dark:border-white rounded-md"
+      >
+        Toggle Dark Mode
+      </button>
+      </div>
+    </div>
+  );
+};
+
+export default Emailheader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
+import store from './state/store.js';
 
 import SplashScreen from './screens/SplashScreen.jsx';
 import SignupPage from './screens/SignupPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter>
-    <NextUIProvider>
-  <Routes>
-      <Route path="/" element={<SplashScreen />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
-    </NextUIProvider>
-   </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <NextUIProvider>
+          <Routes>
+            <Route path="/" element={<SplashScreen />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+        </NextUIProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
