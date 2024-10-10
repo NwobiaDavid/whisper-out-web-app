833f4b1801234e40a2a7c7bc707b88dee14f220e
NwobiaDavid <dnwobia@gmail.com>
2024-10-10 13:32:36 +0100

    feat: made the user interest page responsive

--- src/screens/UsersInterests.tsx
+++ src/screens/UsersInterests.tsx
@@ -77,19 +77,19 @@ const UsersInterests = () => {
 
   return (
     <div
-      className={`w-full max-h-screen h-screen relative bg-cover bg-no-repeat bg-[url('/assets/images/bg/auth.png')] dark:bg-[url('')] dark:bg-maindark`}
+      className={`w-full max-h-screen h-screen relative bg-cover bg-no-repeat bg-[url('')] lg:bg-[url('/assets/images/bg/auth.png')] dark:bg-[url('')] dark:bg-maindark`}
     >
-      <div className="w-full h-[90%] flex xl:py-28 items-center flex-col ">
-        <div className="flex flex-col items-center mb-5">
+      <div className="w-full h-[93%] px-3 lg:px-0 lg:h-[90%] flex py-7 lg:py-20 xl:py-28 items-center flex-col ">
+        <div className="flex flex-col items-center mb-3 lg:mb-5">
           <Image src="/assets/logo1.png" className="h-[60px]" alt="company logo" />
-          <h1 className="my-5 text-2xl font-bold">
+          <h1 className="my-5 text-2xl text-center font-bold">
             What’s the most important things about your company?
           </h1>
           <h3 className="font-semibold text-lg">Select your strong 5</h3>
         </div>
 
-        <form className="flex flex-col items-center mt-28 xl:w-[60%]" onSubmit={handleSubmit}>
-          <div className="grid w-full xl:grid-cols-3 gap-3 mb-10 grid-cols-1 ">
+        <form className="flex flex-col items-center mt-16 lg:mt-28 w-full xl:w-[60%]" onSubmit={handleSubmit}>
+          <div className="grid w-full lg:grid-cols-3 gap-3 mb-5 lg:mb-10 md:grid-cols-2 grid-cols-1 ">
             {interest.map((item, index) => (
               <div
                 key={index}
@@ -120,7 +120,7 @@ const UsersInterests = () => {
               value.length !== 0
                 ? "dark:bg-[#FFC157] dark:text-black dark:hover:bg-[#f1b54d] bg-[#FFC157] hover:bg-[#f1b54d] text-white"
                 : "bg-blue-100 hidden"
-            } p-3 rounded-lg active:scale-95 dark:bg-[#BBC0CA6E] flex justify-center items-center duration-200 font-semibold w-1/2 mt-5`}
+            } p-3 rounded-lg active:scale-95 dark:bg-[#BBC0CA6E] flex justify-center items-center duration-200 font-semibold w-1/2 mt-3 lg:mt-5`}
           >
             Continue
           </button>
