af350214903971639537f66cf5170b546ed046d7
NwobiaDavid <dnwobia@gmail.com>
2024-10-10 13:02:04 +0100

    feat: made the finishsignup page responsive

--- src/screens/FinishSignUp.tsx
+++ src/screens/FinishSignUp.tsx
@@ -110,33 +110,28 @@ const FinishSignUp: React.FC  = () => {
 
   return (
     <div
-      className={`w-full max-h-screen h-screen relative bg-cover bg-no-repeat bg-[url('/assets/images/bg/auth.png')] dark:bg-[url('')] dark:bg-maindark `}
+      className={`w-full max-h-screen h-screen relative bg-cover bg-no-repeat bg-[url('')] lg:bg-[url('/assets/images/bg/auth.png')] dark:bg-[url('')] dark:bg-maindark `}
     >
       <Emailheader />
 
-      <div className="w-full h-[75%] flex justify-center">
+      <div className="w-full h-[83%] lg:h-[75%]  py-10 lg:py-0 flex justify-center">
         {/* {isLoading ? (
           <div className="w-full h-full flex justify-center items-center">
             <Spinner />
           </div>
         ) : ( */}
           <div className="w-full h-full justify-center flex">
-            <div className="w-[70%] border-2 rounded-lg relative">
+            <div className=" w-[85%] lg:w-[70%] border-none lg:border-2 rounded-lg relative">
               <div className="w-full text-lg h-full absolute z-[999] backdrop-blur-sm bg-slate-900 bg-opacity-5 justify-center items-center flex flex-col">
-                {isLoading ? (
-                  <Spinner />
-                ) : (
-                  <div>
-                    <h1 className="font-semibold">Sign-In Confirmation</h1>
-                    <p>{message}</p>
-                  </div>
-                )}
+                {/* {isLoading && ( */}
+                  <Spinner size={"sm"} />
+                {/* // )} */}
               </div>
 
-              <div className="xl:px-20 xl:pt-20 flex xl:w-[65%] flex-col">
-                <div className="text-3xl mb-7 font-bold">
+              <div className="xl:px-20 p-2 xl:pt-20 flex xl:w-[65%] flex-col">
+                <div className="lg:text-3xl text-2xl  mb-7 font-bold">
                   <h1>Just sent you an email</h1>
-                  <h1>
+                  <h1 className=' text-xl  lg:text-3xl' >
                     Check your inbox to confirm your email address and continue
                   </h1>
                 </div>
@@ -144,7 +139,7 @@ const FinishSignUp: React.FC  = () => {
                   We’ve sent a confirmation email to{' '}
                   <span className="font-bold">{email}</span>
                 </p>
-                <div className="mt-5 rounded-lg text-sm drop-shadow-md bg-white dark:bg-inherit px-7 py-5 w-[67%]">
+                <div className="mt-5 rounded-lg text-sm drop-shadow-md bg-white dark:bg-inherit px-5 lg:px-7 py-4 lg:py-5 w-full lg:w-[67%]">
                   <div className="flex font-semibold mb-4 items-center">
                     <FiInfo className="text-lg" />{' '}
                     <span className="ml-3">Didn’t receive an email?</span>
